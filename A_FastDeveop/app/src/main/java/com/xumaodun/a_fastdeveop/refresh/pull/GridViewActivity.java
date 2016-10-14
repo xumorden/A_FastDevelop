74fb876073bd08ae4d6c4d5bb647e26b0750b217
许矛盾 <748838076@qq.com>
2016-10-14 09:56:42 +0800

    修改加载更多

--- A_FastDeveop/app/src/main/java/com/xumaodun/a_fastdeveop/refresh/pull/GridViewActivity.java
+++ A_FastDeveop/app/src/main/java/com/xumaodun/a_fastdeveop/refresh/pull/GridViewActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -38,6 +39,7 @@ public class GridViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_gridview);
 //        initData();
         initView();
